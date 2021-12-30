e57744857e44b65199daac1452937f12c909c6a9
Max Liebe <amalox@outlook.com>
2021-12-30 23:01:43 +0100

    astu ysbrandje

---
+++ levels/tilesets/mario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="mario" tilewidth="16" tileheight="16" tilecount="8" columns="8">
+ <image source="../../imgs/player/shroomaddict.png" width="128" height="16"/>
+</tileset>
