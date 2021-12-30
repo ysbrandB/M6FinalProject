371586ad5d27c9572e2584baebe852a882a4960e
ysbrandB <ysbrand@burgstede.eu>
2021-12-31 00:52:44 +0100

    Added window resizing
    Note: still do this properly with vectors/arrays instead of two seperate variables

--- code/main.py
+++ code/main.py
@@ -1,16 +1,26 @@
-import pygame
 import sys
+
+import pygame
 
 from game_data import level_0
 from level import Level
 from settings import *
-
+import numpy as np
 pygame.init()
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+game_screen_ratio = game_screen_width/game_screen_height
+infoObject = pygame.display.Info()
+
+screen = pygame.display.set_mode((432,480), pygame.RESIZABLE)
+game_screen = pygame.Surface((game_screen_width, game_screen_height))
 clock = pygame.time.Clock()
 
-level = Level(level_0, screen)
+resized_width = screen.get_height() * game_screen_ratio
+resized_height=screen.get_height()
+resized_x_offset = (screen.get_width() - resized_width) / 2
+resized_y_offset=0
+
+level = Level(level_0, game_screen)
 player = level.player
 intro = pygame.mixer.Sound('../sfx/intro.wav')
 intro.play()
@@ -20,7 +30,7 @@ target_fps = 165
 while 1:
     dt = clock.tick(target_fps) * 0.1
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
@@ -35,6 +45,7 @@ while 1:
                     player.jump()
                 case pygame.K_LSHIFT:
                     player.speed_multiplier = 1.5
+
         elif event.type == pygame.KEYUP:
             match event.key:
                 case pygame.K_a:
@@ -47,6 +58,18 @@ while 1:
                         player.jumping = False
                 case pygame.K_LSHIFT:
                     player.speed_multiplier = 1.0
+        elif event.type == pygame.VIDEORESIZE:
+            resized_width = screen.get_height() * (game_screen_width / game_screen_height)
+            resized_height = screen.get_height()
+            resized_y_offset=0
+            if(resized_width>screen.get_width()):
+                resized_width=screen.get_width()
+                resized_height=resized_width/game_screen_ratio
+                resized_y_offset = (screen.get_height()-resized_height)/2
+            resized_x_offset = (screen.get_width() - resized_width) / 2
+
     screen.fill('BLACK')
     level.run(dt)
+    resized_screen = pygame.transform.scale(game_screen, (resized_width, resized_height))
+    screen.blit(resized_screen, (resized_x_offset, resized_y_offset))
     pygame.display.update()
--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,5 @@
 horizontal_tile_number = 27
 vertical_tile_number = 30
 tile_size = 16
-screen_height = vertical_tile_number * tile_size
-screen_width = horizontal_tile_number * tile_size
+game_screen_height = vertical_tile_number * tile_size
+game_screen_width = horizontal_tile_number * tile_size
